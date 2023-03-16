286404ec909d239bd53e2af262e0db43f982ef7e
1gizmo <126743255+1gizmo@users.noreply.github.com>
2023-03-16 16:28:26 +0900

    사칙연산 최종

--- 사칙연산1.js
+++ 사칙연산1.js
@@ -68,13 +68,15 @@ while (true) {
     while(true){
 
         // 사용자에게 문제를 내고 정답을 입력받기
-        var userAnswer = +prompt(`Q${questionNumber++}. ${firstNumber} ${mark} ${secondNumber} = ??`);
+        var userAnswer = prompt(`Q${questionNumber++}. ${firstNumber} ${mark} ${secondNumber} = ??`);
         
         //아무것도 입력안하면 다시 입력
         if(userAnswer === ''){
             continue;
+        } else {
+            userAnswer = +userAnswer;
+            break;
         }
-        break;
     }   
         
     // 게임 종료 조건
